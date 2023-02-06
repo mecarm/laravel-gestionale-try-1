333608f23d1b5ef0b1941036183335c9b7142f39
mecarm <melecarmine99@gmail.com>
2023-02-06 17:33:40 +0100

    Fix bug route show and added missing param

--- routes/web/prospects.php
+++ routes/web/prospects.php
@@ -12,7 +12,7 @@ use App\Http\Controllers\Admin\Prospects\ProspectsController;
 Route::get('/', [ProspectsController::class, 'index'])->name('dashboard');
 Route::get('create', [ProspectsController::class, 'create'])->name('create');
 Route::get('{prospect}/edit', [ProspectsController::class, 'edit'])->where('prospect', '[0-9]+')->name('edit');
-Route::get('{prospect}', [ProspectsController::class, 'show'])->where('prospect', '[0-9]+')->name('show');
+Route::get('{prospect}/show', [ProspectsController::class, 'show'])->where('prospect', '[0-9]+')->name('show');
 Route::delete('{prospect}', [ProspectsController::class, 'destroy'])->where('prospect', '[0-9]+')->name('delete');
 
 //Prospects/prospect/ crud
