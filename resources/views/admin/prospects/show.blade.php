d8e17e84b5b54127abf08aa9299671c252427f34
mecarm <melecarmine99@gmail.com>
2023-02-07 16:12:42 +0100

    layout show prospect

--- resources/views/admin/prospects/show.blade.php
+++ resources/views/admin/prospects/show.blade.php
@@ -21,10 +21,24 @@
                             </ul>
                           </div>
                     </div>
-
                 </div>
+            </div>
+        </div>
 
-                
+        <div class="card mt-3">
+            <div class="card-body row">
+                <div class="col-sm-4">
+                    {{--  Placeholder for image and option to change out just the image --}}
+                    @if ($prospect->profile_image)
+                        <img class="img-fluid" src="{{ Storage::url($prospect->profile_image) }}" alt="">    
+                    @else
+                        <img class="img-fluid" src="/img/user.png" alt="">
+                    @endif
+                </div>
+                <div class="col-sm-8">
+                    <h3>{{$prospect->name}}</h3>
+                    <h5>{{$prospect->email}}</h5>
+                </div>
             </div>
         </div>
     </div>
